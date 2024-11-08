1ff32e44f6cf7777f542322389838478aa2ef193
Gokkull04 <gokkull04@gmail.com>
2024-11-08 13:44:38 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import HomePage from './pages/Home';
 import Profile from './pages/Profile';
-import ForgotPassword from './pages/ForgotPassword';
 import Signup from './pages/Signup';
 import MintRandomNFT from './components/MintRandomNFT'
- 
-import HomePage from './pages/Home'; // Make sure to adjust the import path if needed
+import HomePage from './pages/Home';
  
 const App = () => {
   return (
-  
-    <div>
-        <Home />
-        <MintRandomNFT />
-     </div>
- 
-    
+    <Router>
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <HomePage />
+                <MintRandomNFT />
+              </div>
+            }
+          />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 

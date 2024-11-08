79b3aba2d99e118d4777a2a5d31c0f92ad093486
Gokkull04 <gokkull04@gmail.com>
2024-11-08 12:31:28 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Home'; // Make sure to adjust the import path if needed
+import HomePage from './pages/Home';
+import Profile from './pages/Profile';
+import ForgotPassword from './pages/ForgotPassword';
+import Signup from './pages/Signup';
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Define the route for the HomePage */}
         <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
